5220b5516c92168485727f687b904c1e0dc856c9
chocominto <suiyueweiqi@gmail.com>
2026-05-08 09:55:26 +0900

    初期ファイルの追加: HTML、CSS、JavaScript、画像ファイルを作成

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const photo = document.querySelector("#photo"); // ← 画像を取得
+const fibtn = document.getElementById("FiBtn"); // ボタン取得
+const fobtn = document.getElementById("FoBtn")
+
+const keyframes = {
+    opacity: [0, 1],
+    translate: [0,"150px 150px"]
+};
+const keyframesOut = {
+    opacity: [1,0],
+    translate: [0,"-150px -150px"]
+};
+
+const option = {
+    duration: 1000,
+    fill: "forwards"
+};
+fibtn.onclick = () => {
+    photo.animate(keyframes, option);
+};
+fobtn.onclick = () => {
+    photo.animate(keyframesOut, option);
+};
